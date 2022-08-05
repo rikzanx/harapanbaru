c1f0a224ab3a9866e077e1a6087b77146a61700e
rikzanx <erik.kasbam@gmail.com>
2022-08-05 20:40:45 +0700

    fix invoice

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -110,6 +110,19 @@
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ @rupiah($invoices->sum("profit")) }}</h3>
+                <p>Orderan</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-cart"></i>
+              </div>
+              <a href="{{ route('slider.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
